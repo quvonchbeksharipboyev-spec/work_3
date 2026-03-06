b3ae31545d19f3f9ab87cc2c62032465dd2cee29
quvonchbeksharipboyev-spec <quvonchbeksharipboyev@gmail.com>
2026-03-06 09:10:44 +0300

    Add files via upload

---
+++ work-16.py
@@ -0,0 +1,73 @@
+#1-misol
+#a = int(input("Son kiriting: "))
+#if a > 0:
+#    a = a + 1
+#print(a)
+
+#2-misol
+#a = int(input("Son kiriting: "))
+#if a > 0:
+#    a = a + 1
+#else:
+#    a = a - 2
+#print(a)
+
+
+#3-misol
+#a = int(input("Son kiriting: "))
+#if a > 0:
+#    a = a + 1
+#elif a < 0:
+#    a = a - 2
+#else:
+#    a = 10
+#print(a)
+
+
+#4-misol
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#count = 0
+#if a > 0:
+#    count += 1
+#if b > 0:
+#    count += 1
+#if c > 0:
+#    count += 1
+#print(count)
+
+#5-misol
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#musbat = 0
+#manfiy = 0
+#if a > 0:
+#    musbat += 1
+#elif a < 0:
+#    manfiy += 1
+#if b > 0:
+#    musbat += 1
+#elif b < 0:
+#    manfiy += 1
+
+#if c > 0:
+#    musbat += 1
+#elif c < 0:
+#    manfiy += 1
+#print("Musbat:", musbat)
+#print("Manfiy:", manfiy)
+
+#6-misol
+#a = int(input())
+#b = int(input())
+#if a > b:
+#    print(a)
+#else:
+#    print(b)
+
+
+
+
+
